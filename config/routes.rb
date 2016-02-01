8eaf1c92a0582199f5e90c0decef2b7a0a42d701
김재영 <JYKIM@gimjaeyeongui-MacBook-Pro.local>
2016-02-01 18:59:36 +0900

    title 3ff39c3 코맨트 해결
    1. 먼저 config/route 파일에 resource :stuffs 코드를 따로 하나 추가한다.
    2. route파일에 nested resource 안에 resource :stuffs 코드가
    있으므로scaffold 로 만든 링크 주소도 수정을 해줘야 한다.
    3. app/views/stuffs/index.html.erb 에 링크들을 라우트에 맞게 수정했다.
        <td><%= link_to 'Show', garrage_stuff_path(stuff.garrage,stuff) %></td>
        <td><%= link_to 'Edit', edit_garrage_stuff_path(stuff.garrage,stuff) %></td>
        <td><%= link_to 'Destroy', garrage_stuff_path(stuff.garrage,stuff), method: :delete, data: { confirm: 'Are you
        sure?' } %></td>
    4. 상세 stuff를 보여주는 페이지와 전체 stuffs를 보여주는 페이지를 하나의
    컨트롤러인 stuffs, index액션에서 처리를 해주기 위해서 index액션 내에
    분기문을 사용해서 상세페이지의 경우와 전체를 보여주는 경우를 구분한다.
        def index
          @garrage = Garrage.find_by(params[:garrage_id])
          if(params[:garrage_id].present?
            @stuffs = @garrage.stuffs
          else
            @stuffs = Stuffs.all
          end
        end
    
        -find_by를 쓰는 이유는 nil값일때 에러를 띄우지 않고 if문에 사용하기
        위해서 이다. 만약 find를 쓰면 nil값에 에러를 띄운다.
        -garrage_id가 할당이 되있지 않은 stuff를 전체보기 페이지에 불러오는
        코드.

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,13 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
   root 'home#index'
+
+  resources :stuffs, only: [:index]
+
   resources :garrages do
     resources :stuffs
   end
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
