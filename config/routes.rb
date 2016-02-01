4fd457d03b99a21a5512d726706fec9ff993c035
ISEOB KIM <dltjq0730@unist.ac.kr>
2016-02-01 11:21:04 +0900

    indent fix

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
 
   root 'home#index'
   resources :garrages do
-  	resources :stuffs
-	end
+    resources :stuffs
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
