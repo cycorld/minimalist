20f759be057d35328b7622ab55f59ab6db1e4ae7
Son <sbw@plurry.net>
2016-02-03 12:45:28 +0900

    구글 지도 크기 조절
     - 메인 뷰 구글 지도 크기 조절
     - 메인 뷰에 개러지 리스트 보여줌
     - 지도 클릭시 새로운 개러지 작성 페이지로 넘어가는 것을 넘어가지 않도록 재설정

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,75 @@
 class HomeController < ApplicationController
+  before_action :set_garrage, only: [:show, :edit, :update, :destroy]
+
+  # GET /garrages
+  # GET /garrages.json
   def index
+    @garrages = Garrage.all
   end
+
+  # GET /garrages/1
+  # GET /garrages/1.json
+  def show
+  end
+
+  # GET /garrages/new
+  def new
+    @garrage = Garrage.new
+  end
+
+  # GET /garrages/1/edit
+  def edit
+  end
+
+  # POST /garrages
+  # POST /garrages.json
+  def create
+    @garrage = Garrage.new(garrage_params)
+    @garrage.user_id = current_user.id
+
+    respond_to do |format|
+      if @garrage.save
+        format.html { redirect_to @garrage, notice: 'Garrage was successfully created.' }
+        format.json { render :show, status: :created, location: @garrage }
+      else
+        format.html { render :new }
+        format.json { render json: @garrage.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /garrages/1
+  # PATCH/PUT /garrages/1.json
+  def update
+    respond_to do |format|
+      if @garrage.update(garrage_params)
+        format.html { redirect_to @garrage, notice: 'Garrage was successfully updated.' }
+        format.json { render :show, status: :ok, location: @garrage }
+      else
+        format.html { render :edit }
+        format.json { render json: @garrage.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /garrages/1
+  # DELETE /garrages/1.json
+  def destroy
+    @garrage.destroy
+    respond_to do |format|
+      format.html { redirect_to garrages_url, notice: 'Garrage was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_garrage
+      @garrage = Garrage.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def garrage_params
+      params.require(:garrage).permit(:address, :latitude, :longitude)
+    end
 end
